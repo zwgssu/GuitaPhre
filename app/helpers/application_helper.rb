5afcabae950fba1f0135cd464d87549dd8eb67af
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-15 18:38:14 +0900

    userpage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def cheak_use_picture(user)
+    if user.profile_picture.attached?
+      link_to (image_tag user.profile_picture.variant(gravity: :center, resize:"640x640^", crop:"640x640+0+0").processed, alt: "icon", class: "img-fluid rounded-circle"), user_path(user)
+    else
+      link_to (image_tag "pika.jpeg", alt: "icon", class: "img-fluid rounded-circle"), user_path(user)
+    end
+  end
+
+  def page_title
+    title = "ギタフレ！"
+    title = @page_title + " - " + title if @page_title
+    title
+  end
 end
