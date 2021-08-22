4ae6694af313c33a335eb42f7760c19df94742ff
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-23 00:27:30 +0900

    toppage-t-s header-s

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,8 @@
 class TopController < ApplicationController
   def index
     @users = User.limit(6).order('created_at DESC')
-    @phrase = Phrase.limit(4).order("created_at DESC")
+    @phrases = Phrase.includes(:likers).sort {|a,b| b.likers.size <=> a.likers.size}
+    @themes = Theme.limit(4).order("created_at DESC")
+    @random = Phrase.order("RAND()")
   end
 end
