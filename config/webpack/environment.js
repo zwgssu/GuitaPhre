0240cf67f1a808788e73d18a7808e96ed7aadd6b
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-27 00:09:44 +0900

    webpacker syntax -fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,17 +4,7 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
-
-
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
 )
