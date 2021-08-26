aeacb2e43a48e890f77571025c8b5c1dcaced14a
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-27 00:04:58 +0900

    webpacker-fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,10 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "bootstrap"
 import "../stylesheets/application"
+
+import 'bootstrap';
+import '../stylesheets/application';
 
 require("jquery")
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,4 +9,14 @@ environment.plugins.prepend('Provide',
 )
 
 
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
 module.exports = environment
