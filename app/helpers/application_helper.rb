463d36bc33e457e20aaa065e9a4ff1c9aa25c554
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-28 00:57:23 +0900

    deploy ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def cheak_use_picture(user, size)
     if user.profile_picture.attached?
-      link_to (image_tag user.profile_picture, size: size, alt: "icon", class: "img-fluid rounded-circle"), user_path(user)
+      link_to (image_tag user.profile_picture.variant(gravity: :center, resize:"640x640^", crop:"640x640+0+0").processed, size: size, alt: "icon", class: "img-fluid rounded-circle"), user_path(user)
     else
-      link_to (image_pack_tag "20227.jpg", alt: "icon", size: size, class: "img-fluid rounded-circle"), user_path(user)
+      link_to (image_tag "20227.jpg", alt: "icon", size: size, class: "img-fluid rounded-circle"), user_path(user)
     end
   end
 
