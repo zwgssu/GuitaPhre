e03ad367e34428ab9e8af13557b601bf86c3ff73
川崎和真 <h-150@kawasakiwashinnoMacBook-Air.local>
2021-08-31 00:17:53 +0900

    toppage-fix search-comment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def search
-    @model = params["search"]["model"]
+    @model = params["search"]["model"] #serchformからmodelとvalueを取得する
     @value = params["search"]["value"]
+
+    #modelの中身に合わせて分岐させ、タイトルとタグ、もしくはユーザー名をvalueをもとにデータを取得しdatesに入れる
     if @model == "phrase"
       @dates = Phrase.where("title LIKE ? OR tag_1 LIKE ? OR tag_2 LIKE ? OR tag_3 LIKE ? OR body LIKE ?", "%#{@value}%", "%#{@value}%", "%#{@value}%", "%#{@value}%", "%#{@value}%").page(params[:page]).per(6).order('created_at DESC')
     elsif @model == "theme"
